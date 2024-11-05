b53237a4d5c29dcc7321258cd66185d7f4e8829b
SprangerjS <sebi.spranger@gmail.com>
2024-11-05 12:07:28 +0100

    initial commit

---
+++ observations/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class CelestialTarget(models.Model):
+    name = models.CharField(max_length=100)
+    coordinates = models.CharField(max_length=100)
+
+class AbstractObservation(models.Model):
+    observatory = models.CharField(max_length=100)
+    target = models.ForeignKey(CelestialTarget, on_delete=models.CASCADE, related_name='observations')
+
+class ExoplanetObservation(AbstractObservation):
+    filter_set = models.CharField(max_length=100)
+
+
---
+++ observations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from interactiveTest.views import input_test, create_json
+from observations.views import create_observation
+
+urlpatterns = [
+    path('create/', create_observation),
+]
