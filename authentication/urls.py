75406089ccf74bfa75c68ddf65b820562298a080
Bennet Bo Fenner <bennet@zed.dev>
2024-11-15 09:04:43 +0100

    allow users to logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from authentication.views import index, generate_invitation_link, register
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", index, name="index" ),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
     path("generate-invitation-link", generate_invitation_link, name="generate-invitation-link"),
     path('register/<uuid:token>/', register, name='register_from_invitation'),
 ]
