e410465b6e7f476fca085e3ce8f8cbef3f398a34
Keyscha Gold <115990269+Keyscha@users.noreply.github.com>
2024-11-28 21:00:23 +0100

    Remove unrelated changes

--- TURMFrontend/settings.py
+++ TURMFrontend/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS
 from dotenv import load_dotenv
 
 
