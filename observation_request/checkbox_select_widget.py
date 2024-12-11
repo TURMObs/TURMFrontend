5e64054ef62cd8ac935db71c13aac3b29f4c538f
SprangerjS <sebastian.spranger@stud.tu-darmstadt.de>
2024-12-11 12:08:28 +0100

    fixed filter set spacing

--- observation_request/checkbox_select_widget.py
+++ observation_request/checkbox_select_widget.py
@@ -30,11 +30,14 @@ class TURMCheckboxSelectWidget(Widget):
         i = 0
 
         for q in self.queryset:
-            out += (f'<div class="multiple_choice_div{" tooltip_div" if self.tooltip is not None else ""}"> <input type="checkbox" name="{name}" value="{q}" '
+            out += (f'<div class="{" tooltip_div" if self.tooltip is not None else ""}"> '
+                    f'<div class="multiple_choice_div">'
+                    f'<input type="checkbox" name="{name}" value="{q}" '
                     f'id="id_{name}_{i}" {html_attributes} '
                     f'{self.extra_attribute_factory(q) if self.extra_attribute_factory is not None else ""}>'
                     f'<label for="id_{name}_{i}"> {q} </label>'
-                    f'{self._render_tooltip(f"id_{name}_{i}") if self.tooltip is not None else ""}</div>')
+                    f'{self._render_tooltip(f"id_{name}_{i}") if self.tooltip is not None else ""}'
+                    f'</div></div>')
             i += 1
         out += '</div>'
 
