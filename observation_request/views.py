cdf6cff9ffc6a723a4d75a9ebc7e142b8ddeda7c
SprangerjS <sebastian.spranger@stud.tu-darmstadt.de>
2024-12-17 12:48:14 +0100

    removed old forms

--- observation_request/views.py
+++ observation_request/views.py
@@ -7,50 +7,8 @@ from rest_framework.decorators import api_view
 from observation_data.models import ObservationType, Observatory, AbstractObservation, ExpertObservation
 from observation_data.forms import (
     CelestialTargetForm,
-    ExposureForm,
-    QueryEnum, WipForm, ExposureSettingsForm, TRUMProjectForm
+    QueryEnum, ExposureSettingsForm, TRUMProjectForm
 )
-
-
-def _simple_request(request):
-    context = {}
-    # Observation Types
-    # list of Name and if they should only be displayed to a super_user
-    print(Observatory.objects.first().pk)
-    observatories_radio = {
-        "options": ((obs.pk, False) for obs in Observatory.objects.all()),
-        "hide": "false",
-        "name": "observatory",  # todo: change to model meta field name
-        "input_selector": "observatory_radio_label",
-        "effect_selector": QueryEnum.observatory_dependent.name,
-    }
-    observation_types_radio = {
-        "options": (
-            (str(ObservationType.IMAGING), False),
-            (str(ObservationType.EXOPLANET), False),
-            (str(ObservationType.VARIABLE), False),
-            (str(ObservationType.MONITORING), False),
-            (str(ObservationType.EXPERT), True),
-        ),
-        "hide": "true",
-        "name": "observation_type",
-        "input_selector": "observation_type_radio_label",
-        "effect_selector": QueryEnum.observation_type_dependent.name,
-    }
-
-    # Forms
-    c_forms = [
-        ("Project", "", observatories_radio),
-        ("Target", CelestialTargetForm(), None),
-        ("Exposure", ExposureForm(), observation_types_radio),
-        ("", WipForm(), None),
-    ]
-    context["forms"] = c_forms
-
-    # endpoint url
-    context['create_form_url'] = '../observation-data/create/'
-
-    return render(request, "observationRequest/requestTemplate.html", context)
 
 def simple_request(request):
     context = {}
