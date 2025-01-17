cf7e2f63f4bbd6487324cced67adb5d41d044d3a
Bennet Bo Fenner <bennet@zed.dev>
2025-01-17 10:38:22 +0100

    fix

--- observation_data/tests.py
+++ observation_data/tests.py
@@ -93,13 +93,13 @@ class ObservationCreationTestCase(django.test.TestCase):
     def test_invalid_type(self):
         response = self._send_post_request({"observation_type": "Invalid"})
         self._assert_error_response(
-            response, 400, {"error": 'Invalid observation type: "Invalid"'}
+            response, 400, {"error": "Invalid observation type: Invalid"}
         )
 
     def test_invalid_type_flat(self):
         response = self._send_post_request(self._get_flat_base_request())
         self._assert_error_response(
-            response, 400, {"error": 'Invalid observation type: "Invalid"'}
+            response, 400, {"error": "Invalid observation type: Invalid"}
         )
 
     def test_missing_fields(self):
