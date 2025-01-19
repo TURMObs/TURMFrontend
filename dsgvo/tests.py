6f4511c50eaf173d44eba8a21134820665c0ff75
jwick <josias.wick@stud.tu-darmstadt.de>
2025-01-19 18:33:46 +0100

    Tried to fix CI #9

--- dsgvo/tests.py
+++ dsgvo/tests.py
@@ -25,8 +25,10 @@ from observation_data.models import (
 run_nc_test = False if os.getenv("NC_TEST", default=True) == "False" else True
 
 
-@unittest.skipIf(not run_nc_test,
-                 "Nextclouds test cannot run in CI. Set env variable `NC_TEST=True` to run nextcloud tests.")
+@unittest.skipIf(
+    not run_nc_test,
+    "Nextclouds test cannot run in CI. Set env variable `NC_TEST=True` to run nextcloud tests.",
+)
 class DSGVOUserDataTestCase(django.test.TestCase):
     def setUp(self):
         initialize_connection()
