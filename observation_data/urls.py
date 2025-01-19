a566f9a93c4d766d87051d2a4fd013fe99d11371
Bennet Bo Fenner <bennet@zed.dev>
2025-01-19 23:51:23 +0100

    remove unrelated changes

--- observation_data/urls.py
+++ observation_data/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from observation_data.views import create_observation, delete_observation
+from observation_data.views import create_observation
 
 urlpatterns = [
     path("create/", create_observation),
-    path("delete/<int:observation_id>/", delete_observation),
 ]
