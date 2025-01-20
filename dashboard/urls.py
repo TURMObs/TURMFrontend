03bd25e74b5762283ba63ee20e7e11a835133cca
Bennet Bo Fenner <bennet@zed.dev>
2025-01-20 22:41:04 +0100

    Introduce formatting error to test action

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from dashboard.views import dashboard
 from django.conf import settings
 
 urlpatterns = [
-    path("", dashboard, name=settings.LOGIN_REDIRECT_URL),
+    path("", 
+        dashboard, name=settings.LOGIN_REDIRECT_URL),
 ]
