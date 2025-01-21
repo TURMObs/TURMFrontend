f224ab44f9d906d4ee44294bc5003e217bd35af9
SprangerjS <sebastian.spranger@stud.tu-darmstadt.de>
2025-01-21 13:39:55 +0100

    WIP added debug buttons

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from dashboard.views import dashboard
+from dashboard.views import dashboard, delete_all, upload_observation, update_observation
 from django.conf import settings
 
 urlpatterns = [
     path("", dashboard, name=settings.LOGIN_REDIRECT_URL),
+    path("delete-observations/", delete_all),
+    path("upload-observations/", upload_observation),
+    path("update-observations/", update_observation),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.core.management import call_command
+from django.shortcuts import render, redirect
 from observation_data.models import AbstractObservation
 
 
@@ -9,3 +10,15 @@ def dashboard(request):
         observations = AbstractObservation.objects.filter(user=request.user)
 
     return render(request, "dashboard/index.html", {"observations": observations})
+
+def delete_all(request):
+    AbstractObservation.objects.all().delete()
+    return redirect("dashboard")
+
+def upload_observation(request):
+    call_command("upload_observations")
+    return redirect("dashboard")
+
+def update_observation(request):
+    call_command("update_observations")
+    return redirect("dashboard")
