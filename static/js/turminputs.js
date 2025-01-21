677f9724a669b438d238dddeaf25758a7646864a
Bennet Bo Fenner <bennet@zed.dev>
2025-01-21 11:25:09 +0100

    Revert "formatting"
    This reverts commit a72f4b75c0b634400a1a549fa8b2622552d8c650.

--- static/js/turminputs.js
+++ static/js/turminputs.js
@@ -6,9 +6,9 @@
  */
 
 function discard_input(event, el, suppressed) {
-  el.value = el.value.replace(RegExp(suppressed), "");
-  event.stopPropagation();
-  event.preventDefault();
+    el.value = el.value.replace(RegExp(suppressed), '');
+    event.stopPropagation();
+    event.preventDefault();
 }
 
 /**
@@ -16,22 +16,21 @@ function discard_input(event, el, suppressed) {
  * Is supposed to be called on page load
  */
 function check_radio_if_none_selected() {
-  const divs = document.getElementsByClassName("radio_input_div");
-  for (let div of divs) {
-    const radios = Array.from(div.children)
-      .filter((el) => el.tagName === "INPUT")
-      .filter((el) => el.type === "radio");
-    let selected_or_first_radio = radios[0];
-    for (let radio of radios) {
-      if (radio.checked === true) {
-        selected_or_first_radio = radio;
-        break;
-      }
+    const divs = document.getElementsByClassName('radio_input_div');
+    for (let div of divs) {
+        const radios = Array.from(div.children)
+            .filter(el => el.tagName === 'INPUT')
+            .filter(el => el.type === 'radio');
+        let selected_or_first_radio = radios[0]
+        for (let radio of radios) {
+            if (radio.checked === true) {
+                selected_or_first_radio = radio;
+                break;
+            }
+        }
+        selected_or_first_radio.checked = true;
+        if (selected_or_first_radio.onclick != null) selected_or_first_radio.click()
     }
-    selected_or_first_radio.checked = true;
-    if (selected_or_first_radio.onclick != null)
-      selected_or_first_radio.click();
-  }
 }
 
 /**
@@ -41,18 +40,17 @@ function check_radio_if_none_selected() {
  * @param dependency
  */
 function disable_inputs(dependency_type, dependency) {
-  const dependent_inputs = Array.from(
-    document.getElementsByTagName("INPUT"),
-  ).filter((el) => !!el.getAttribute(dependency_type));
+    const dependent_inputs = Array.from(document.getElementsByTagName('INPUT'))
+        .filter(el => !!el.getAttribute(dependency_type));
 
-  for (let input of dependent_inputs) {
-    if (input.getAttribute(dependency_type).split(" ").includes(dependency)) {
-      input.disabled = false;
-    } else {
-      input.disabled = true;
-      input.checked = false;
+    for (let input of dependent_inputs) {
+        if (input.getAttribute(dependency_type).split(' ').includes(dependency)) {
+            input.disabled = false;
+        } else {
+            input.disabled = true;
+            input.checked = false;
+        }
     }
-  }
 }
 
 /**
@@ -62,38 +60,36 @@ function disable_inputs(dependency_type, dependency) {
  * @param dependency
  */
 function hide_inputs(dependency_type, dependency) {
-  const dependent_inputs = Array.from(
-    document.getElementsByTagName("INPUT"),
-  ).filter((el) => !!el.getAttribute(dependency_type));
+    const dependent_inputs = Array.from(document.getElementsByTagName('INPUT'))
+        .filter(el => !!el.getAttribute(dependency_type));
 
-  for (let input of dependent_inputs) {
-    const hide_el = !input.getAttribute(dependency_type).includes(dependency);
-    let parent = input.parentElement;
-    const local_inputs = Array.from(parent.children).filter(
-      (el) => el.tagName === "INPUT",
-    );
-    if (parent.classList.contains("radio_input_div")) {
-      parent = input.parentElement.parentElement;
+    for (let input of dependent_inputs) {
+        const hide_el = !input.getAttribute(dependency_type).includes(dependency)
+        let parent = input.parentElement;
+        const local_inputs = Array.from(parent.children).filter(el => el.tagName === 'INPUT');
+        if (parent.classList.contains("radio_input_div")) {
+            parent = input.parentElement.parentElement;
+        }
+        if (hide_el) {
+            parent.style.display = 'none';
+            for (let l_input of local_inputs) {
+                l_input.disabled = true;
+            }
+        }
+        else {
+            parent.removeAttribute("style");
+            for (let l_input of local_inputs) {
+                l_input.disabled = false;
+            }
+        }
     }
-    if (hide_el) {
-      parent.style.display = "none";
-      for (let l_input of local_inputs) {
-        l_input.disabled = true;
-      }
-    } else {
-      parent.removeAttribute("style");
-      for (let l_input of local_inputs) {
-        l_input.disabled = false;
-      }
-    }
-  }
 }
 
 function update_date_dependency(el) {
-  const end = document.getElementById(el.id.replace(RegExp("start"), "end"));
-  if (end === null) return;
-  end.min = el.value;
-  if (Date.parse(end.min) > Date.parse(end.value)) {
-    end.value = end.min;
-  }
+    const end = document.getElementById(el.id.replace(RegExp('start'), 'end'));
+    if (end === null) return;
+    end.min = el.value;
+    if (Date.parse(end.min) > Date.parse(end.value)) {
+        end.value = end.min;
+    }
 }
