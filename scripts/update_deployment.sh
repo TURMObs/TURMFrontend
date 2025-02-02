f4249a4f2aff697ca996f059cc3cf730206c6d86
Bennet Bo Fenner <bennet@zed.dev>
2025-02-02 20:14:06 +0100

    Update deployment script

--- scripts/update_deployment.sh
+++ scripts/update_deployment.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-git pull
-docker compose down -v
-docker compose up -d --build
+set -e
+
+echo "Fetching newest changes..."
+git fetch origin deployment || exit 1
+git checkout -B deployment origin/deployment || exit 1
+git pull || exit 1
+
+echo "Stopping server..."
+
+docker compose down -v || exit 1
+echo "Starting server..."
+docker compose up -d --build || exit 1
+
+echo "Deployment complete, newest version is up and running."
