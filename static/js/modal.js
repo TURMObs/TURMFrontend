f22b57177ab9c061f50d5f40a9b5011bfbbdb35a
Bennet Bo Fenner <bennet@zed.dev>
2025-02-07 09:52:26 +0100

    WIP more dialogs

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,51 +1,85 @@
-function showModal(
+async function showConfirmModal(title, message, confirmText = "Confirm") {
+  const result = await showModal(title, message, {
+    buttons: [
+      {
+        id: "cancel",
+        text: "Cancel",
+        secondary: true,
+      },
+      {
+        id: "confirm",
+        text: confirmText,
+      },
+    ],
+  });
+  return result === "confirm";
+}
+
+async function showModal(
   title,
   message,
   options = { allowDismiss: true, buttons: [] },
 ) {
-  const modal = document.getElementById("modal");
-  const modalMessage = document.getElementById("modal-message");
-  const modalTitle = document.getElementById("modal-title");
-  const modalSeperator = document.getElementById("modal-seperator");
-  const modalActions = document.getElementById("modal-actions");
+  return new Promise((resolve) => {
+    const modal = document.getElementById("modal");
+    const modalMessage = document.getElementById("modal-message");
+    const modalTitle = document.getElementById("modal-title");
+    const modalSeperator = document.getElementById("modal-seperator");
+    const modalActions = document.getElementById("modal-actions");
 
-  showElement(modal, "block");
-  modalTitle.textContent = title;
-  modalMessage.textContent = message;
+    showElement(modal, "block");
+    modalTitle.textContent = title;
+    modalMessage.textContent = message;
 
-  if (options.buttons && options.buttons.length > 0) {
-    showElement(modalSeperator, "flex");
-    showElement(modalActions, "flex");
-    modalActions.innerHTML = "";
-    for (var button of options.buttons) {
-      const buttonElement = document.createElement("button");
-      if (button.secondary) {
-        buttonElement.className = "btn-secondary";
-      } else {
-        buttonElement.className = "btn";
+    if (options.buttons && options.buttons.length > 0) {
+      showElement(modalSeperator, "flex");
+      showElement(modalActions, "flex");
+      modalActions.innerHTML = "";
+      for (var button of options.buttons) {
+        const buttonElement = document.createElement("button");
+        const buttonId = button.id;
+        if (button.secondary) {
+          buttonElement.className = "btn-secondary";
+        } else {
+          buttonElement.className = "btn";
+        }
+        buttonElement.textContent = button.text;
+        buttonElement.addEventListener("click", () => {
+          button.onClick?.();
+          hideModal();
+          resolve(buttonId);
+        });
+        modalActions.appendChild(buttonElement);
       }
-      buttonElement.textContent = button.text;
-      buttonElement.addEventListener("click", button.onClick);
-      modalActions.appendChild(buttonElement);
+    } else {
+      hideElement(modalSeperator);
+      hideElement(modalActions);
     }
-  } else {
-    hideElement(modalSeperator);
-    hideElement(modalActions);
-  }
 
-  if (options.allowDismiss) {
-    window.onclick = function (event) {
+    const windowClickHandler = function (event) {
       if (event.target === modal) {
-        hideModal();
+        dismissHandler();
       }
     };
 
-    document.addEventListener("keydown", function (event) {
+    const keydownHandler = function (event) {
       if (event.key === "Escape") {
-        hideModal();
+        dismissHandler();
       }
-    });
-  }
+    };
+
+    const dismissHandler = () => {
+      window.removeEventListener("click", windowClickHandler);
+      document.removeEventListener("keydown", keydownHandler);
+      hideModal();
+      resolve(null);
+    };
+
+    if (options.allowDismiss) {
+      window.addEventListener("click", windowClickHandler);
+      document.addEventListener("keydown", keydownHandler);
+    }
+  });
 }
 
 function hideModal() {
