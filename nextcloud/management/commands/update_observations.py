509619fea64c1de2aaeec1521b2a85a9586ec382
Benjamin <57810427+btw04@users.noreply.github.com>
2025-03-10 10:45:19 +0000

    use dates instead of datetimes in commands (#92)

--- nextcloud/management/commands/update_observations.py
+++ nextcloud/management/commands/update_observations.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
             time_delta = options["days"]
 
         try:
-            update_observations(timezone.now() + timedelta(days=time_delta))
+            update_observations((timezone.now() + timedelta(days=time_delta)).date())
         except Exception as e:
             logger.error(f"Error updating observations: {e}")
             self.stdout.write(self.style.ERROR(f"Error updating observations: {e}"))
